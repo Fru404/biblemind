7c6b6038b5c947cfcd4aece370edd523f4d2c8d4
Fru Ngwa <dohng28@gmail.com>
2025-08-04 15:57:28 +0100

    Adding icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div >
-      <main >
-       
+    <div className="flex flex-col min-h-screen bg-gray-100 text-gray-900">
+      <main className="flex-grow ">
+        <h1 className="text-4xl font-bold text-blue-600">biblemind</h1>
       </main>
-      <footer >
-        
+
+      <footer className="p-4 text-center text-sm text-gray-500">
+        BibleMind. All rights reserved.
       </footer>
     </div>
   );
