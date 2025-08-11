e9fe556117f650c9f7cb97efbd4e6d862e86c7ac
Fru Ngwa <dohng28@gmail.com>
2025-08-11 14:14:05 +0100

    Add caching of all https request for offline mode

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,26 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development', // disable PWA in dev
+  disable: process.env.NODE_ENV === 'development', 
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/, // Cache all HTTP/S requests
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offline-cache',
+        expiration: {
+          maxEntries: 200,
+          maxAgeSeconds: 7 * 24 * 60 * 60, // 1 week
+        },
+      },
+    },
+  ],
 });
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  
 };
 
 module.exports = withPWA(nextConfig);
+ 
