7a9ea520e30bf867c0b7189ca75e1ef4bc1c38dd
Fru Ngwa <dohng28@gmail.com>
2025-10-07 15:48:01 +0100

    Fix: unmounted navigation router in about/page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function AboutPage() {
   const router = useRouter();
